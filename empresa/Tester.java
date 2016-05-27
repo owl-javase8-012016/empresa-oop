c4a9e8636852e156d23cbf71c2bca4d7862e41de
amorel <betomor.6@gmail.com>
2016-05-26 21:12:05 -0400

    se agrago el nombre hugo en el tester

--- empresa/Tester.java
+++ empresa/Tester.java
@@ -5,7 +5,9 @@ import empresa.rrhh.Supervisor;
 import empresa.rrhh.Empleado;
 
 public class Tester {
-
+    
+    public static final String VERSION = "1.0.0";
+    public static final String RAPHAPY = "Hugo Gonzalez"
 	private Empleado vendedor;
 	private Empleado supervisor;
 
