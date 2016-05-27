fde4df3a94ca00e39c1f4325bfeb615a130fe9fa
moviedo <miriamoviedo45@gmail.com>
2016-05-26 21:21:49 -0400

    Se agrego mi nombre de usuario

--- empresa/Tester.java
+++ empresa/Tester.java
@@ -5,7 +5,8 @@ import empresa.rrhh.Supervisor;
 import empresa.rrhh.Empleado;
 
 public class Tester {
-
+	public static final String VERSION = "1.0.0";
+	public static final String DEHRLIS = "Derlis Romero"
 	private Empleado vendedor;
 	private Empleado supervisor;
 
