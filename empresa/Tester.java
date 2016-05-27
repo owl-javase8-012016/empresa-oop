939e581d72186bce3efb6ddf22d00c019731bd11
rafael.benegas@konecta.com.py <rafael.benegas@konecta.com.py>
2016-05-26 21:02:43 -0400

    Se agrega versión a la clase Tester

--- empresa/Tester.java
+++ empresa/Tester.java
@@ -5,7 +5,7 @@ import empresa.rrhh.Supervisor;
 import empresa.rrhh.Empleado;
 
 public class Tester {
-
+	public static final String VERSION = "1.0.0";
 	private Empleado vendedor;
 	private Empleado supervisor;
 
